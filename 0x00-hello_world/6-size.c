d6480bb4540b97bad59666c33e5b21832c4e7dee
zekky14 <ezek.ogbeda@gmail.com>
2022-12-11 04:17:56 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
-#include<studio.h>
+#include <studio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int mainn(void)
+int main(void)
 {
   printf("Size of a char: %d byte(s)\n", sizeof(char));
   printf("Size of an int: %d byte(s)\n", sizeof(int));
