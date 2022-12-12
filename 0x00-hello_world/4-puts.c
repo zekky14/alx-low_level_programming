db632d645ed6d5dabb80bc3c8fa04ffe96768474
zekky14 <ezek.ogbeda@gmail.com>
2022-12-11 20:54:57 -0800

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-
 #include <stdio.h>
-   /**
-    * main - Entry Point
-    *
-    * Return: Always 0 (Success)
-    */
+
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -12,5 +13,4 @@ int main(void)
   return (0);
 
 
-  
 }
