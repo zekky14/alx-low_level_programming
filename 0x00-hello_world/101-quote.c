6f5f7c12d1f8ecf98e24264662e8b83b0165d82e
zekky14 <ezek.ogbeda@gmail.com>
2022-12-12 15:42:15 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 /**
 * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
 * followed by a new line, to the standard error.
-* REturn: Always 1.
+* Return: Always 1.
 */
 
 int main(void)
