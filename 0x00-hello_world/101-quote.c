219503bb94e242d7620c7ea303e201d9b0daa3b7
zekky14 <ezek.ogbeda@gmail.com>
2022-12-12 15:31:23 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 
 /**
-main - Print "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+* main - Print "and that piece of art is useful" - Dora Korpar, 2015-10-19,
 * followed by a new line, to the standard error.
 * REturn: Always 1.
 *
@@ -12,5 +12,5 @@ int main(void)
 
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return(1);
+return (1);
 }
