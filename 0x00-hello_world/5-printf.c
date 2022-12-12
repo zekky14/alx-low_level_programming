dcf63f02d36c788fc9c2658dbc912bd7cb33afe3
zekky14 <ezek.ogbeda@gmail.com>
2022-12-11 21:07:29 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
-
 #include <stdio.h>
+
 
 /**
  * main - prints a C program using printf
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 
+ 
 }
