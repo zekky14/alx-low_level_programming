ae97e54151f2138b2e0709dee91b82cb9aa2ab9d
zekky14 <ezek.ogbeda@gmail.com>
2022-12-16 06:55:23 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Print alphabet in lowercase
+* Return: ALways (success)
+*/
+
+int main(void)
+
+{
+
+  char c;
+
+  for (c = 'a'; c <= 'z'; c++);
+  putchar(c);
+  putchar('\n');
+  return (0);
+}  
