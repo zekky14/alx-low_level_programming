34f1030977adf83a607b77d0d20a4d193dea0fb2
zekky14 <ezek.ogbeda@gmail.com>
2022-12-16 18:41:39 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int b;
+for (b = '0'; b < '9'; b++)
+putchar(b);
+putchar('\n');
+
+ return (0);
+
+}
