6f8d66a915d41c6f8e19142d3b28cc4dca03e76f
zekky14 <ezek.ogbeda@gmail.com>
2022-12-18 20:34:49 -0800

    Single digit ascending order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+int a;
+ 
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+if (a != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
