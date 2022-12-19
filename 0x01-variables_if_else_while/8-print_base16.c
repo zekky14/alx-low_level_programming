7ced280d4c5862e8a37b1016c71a05fcb164f649
zekky14 <ezek.ogbeda@gmail.com>
2022-12-18 20:15:40 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+
+int b;
+int low;
+ 
+for (b = '0'; b <= '9'; b++)
+putchar(b);
+putchar('\n');
+
+for (low = 'a'; low <= 'f'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+
+}
