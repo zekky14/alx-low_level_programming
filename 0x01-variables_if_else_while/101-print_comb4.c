e05a69bd25d384a965c49d4d985603b4a010f1f2
zekky14 <ezek.ogbeda@gmail.com>
2022-12-19 18:06:02 -0800

    3 digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+
+int d, p, r;
+
+for (d = '0'; d < '9'; d++)
+{
+for (p = d + 1; p <= '9'; p++)
+{
+for (r = p + 1; r <= '9'; r++)
+{
+if ((p != d) != r )
+{
+putchar(d);
+putchar(p);
+putchar(r);
+if (d == '7' && p == '8')
+continue;
+ putchar(',');
+ putchar(' ');
+ }
+ }
+ }
+ }
+ putchar('\n');
+ return (0);
+}
