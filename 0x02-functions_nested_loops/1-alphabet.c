8b6f8b911c1c4eafa24e3669c1998714d7bbb9de
zekky14 <ezek.ogbeda@gmail.com>
2022-12-20 03:58:26 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+char b;
+for (b = 'a'; b <= 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+return (0);
+}
+  
