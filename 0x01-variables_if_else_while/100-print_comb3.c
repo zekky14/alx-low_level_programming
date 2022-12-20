7fe21f41ae7fa1ef8982831ca88b2ebebece9475
zekky14 <ezek.ogbeda@gmail.com>
2022-12-19 17:24:34 -0800

    Two digits combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point 
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+
+{
+  
+int r,p;
+
+for (r = '0'; r < '9'; r++)
+{
+for ( p = r + 1; p <= '9'; p++)
+{
+if (p != r)
+{
+putchar(r);
+putchar(p);
+if ( r == '8' && p == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
+ 
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,11 +15,10 @@ int a;
 for (a = '0'; a <= '9'; a++)
 {
 putchar(a);
-if (a != 9)
-{
+if (a == 8)
+continue;
 putchar(',');
 putchar(' ');
-}
 }
 putchar('\n');
 return (0);
