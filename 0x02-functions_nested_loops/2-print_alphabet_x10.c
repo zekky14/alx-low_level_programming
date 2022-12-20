f97cee79a5038980ba044b7cbe24d2bb90fddbcf
zekky14 <ezek.ogbeda@gmail.com>
2022-12-20 07:25:54 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * main - alphabets
+ * print_alphabet_x10 , print a-z 10times 
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j ='a';  j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
