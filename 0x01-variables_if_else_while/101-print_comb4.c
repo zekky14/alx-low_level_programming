8d9a5dff754a52750b82f91d9fa02aae09cd4ba5
zekky14 <ezek.ogbeda@gmail.com>
2022-12-19 18:10:11 -0800

    3 digit combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,19 +20,19 @@ for (p = d + 1; p <= '9'; p++)
 {
 for (r = p + 1; r <= '9'; r++)
 {
-if ((p != d) != r )
+if ((p != d) != r)
 {
 putchar(d);
 putchar(p);
 putchar(r);
 if (d == '7' && p == '8')
 continue;
- putchar(',');
- putchar(' ');
- }
- }
- }
- }
- putchar('\n');
- return (0);
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
