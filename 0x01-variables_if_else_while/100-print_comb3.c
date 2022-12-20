090f2b3ba8447406ad2e589680fd22f932f28cf1
zekky14 <ezek.ogbeda@gmail.com>
2022-12-19 17:38:05 -0800

    Two digits combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,24 +4,23 @@
 
 
 /**
-* main - Entry point 
+* main - Entry point
 * Return: Always 0 (success)
 *
 */
 
 int main(void)
-
 {  
-int r,p;
+int r, p;
 for (r = '0'; r < '9'; r++)
 {
-for ( p = r + 1; p <= '9'; p++)
+for (p = r + 1; p <= '9'; p++)
 {
 if (p != r)
 {
 putchar(r);
 putchar(p);
-if ( r == '8' && p == '9')
+if (r == '8' && p == '9')
 continue;
 putchar(',');
 putchar(' ');
