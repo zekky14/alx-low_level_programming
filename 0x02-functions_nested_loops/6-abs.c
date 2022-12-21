ed7c58045b74725701d7a495f1f239af022d4f69
zekky14 <ezek.ogbeda@gmail.com>
2022-12-21 00:10:30 -0800

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ *  @q: int datatype number
+ *  Return: value
+ */
+
+int _abs(int q)
+{
+if (q < 0)
+{
+return (q * -1);
+}
+else
+{
+return (q);
+}
+}
