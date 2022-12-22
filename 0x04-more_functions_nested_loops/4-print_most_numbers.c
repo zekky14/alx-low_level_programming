d6265dc5d97dc5f5e6d782a601dadc521e63ac60
zekky14 <ezek.ogbeda@gmail.com>
2022-12-21 16:07:08 -0800

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,17 +8,17 @@
 
 void print_most_numbers(void)
 {
-  int x = 0;
-  for (; x <= 9; x++)
-    {
-      if (x == 2 || x == 4)
-	{
-	  continue;
-	}
-      else
-	{
-	  _putchar(x + '0');
-	}
-    }
-  _putchar('\n');
+int x = 0;
+for (; x <= 9; x++)
+{
+if (x == 2 || x == 4)
+{
+continue;
+}
+else
+{
+_putchar(x + '0');
+}
+}
+_putchar('\n');
 }
