8fbfb05aace945ce0f6c4eb568e3fb8f9e9841fb
zekky14 <ezek.ogbeda@gmail.com>
2023-01-02 23:31:38 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _memset -Fills the first n  bytes of the memory area
+ * pointed to by @s withthe constant byte @c.
+ * @s: A pointer to the memry area to be filled .
+ * @c: The character to fill the emory are with.
+ * @n: The number of the bytes to be filled.
+ * description _memset: other there
+ *
+ * Return: A pointer to be filled memeory are @s
+ */
+
+char *_memset(char *s, char b, unsigned int n)  
+{
+unsigned int index;
+for (index = 0; index < n; index++)
+s[index] = b;
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x07. C Pointer to pointer project
