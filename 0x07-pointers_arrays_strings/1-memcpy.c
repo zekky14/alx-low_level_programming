2e019c0df6b33ee404ae497fd2e2f982b5bec512
zekky14 <ezek.ogbeda@gmail.com>
2023-01-05 18:40:33 -0800

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry Point
+ * @dest: Input
+ * @src: Input
+ * @n: Input
+ * Return: ways 0. (Success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
