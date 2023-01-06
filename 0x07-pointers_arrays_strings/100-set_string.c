2b26b9c482dc649213f9dcad01b28af932b9fd7c
zekky14 <ezek.ogbeda@gmail.com>
2023-01-05 20:58:37 -0800

    Set string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * set_string : Entry point
- * @s: input
- * @to: input
- * Return: Always 0 (success)
- */
+* set_string : Entry point
+* @s: input
+* @to: input
+* Return: Always 0 (success)
+*/
 void set_string(char **s, char *to)
 {
 *s = to;
