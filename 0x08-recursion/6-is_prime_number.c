4bb175a2c562e026839f1c4e8d4ee5dc21304bd4
zekky14 <ezek.ogbeda@gmail.com>
2023-01-09 07:28:21 -0800

    Prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 
-int check_prime(int n, int i);
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 
 /**
-* _is_prime_number - Returns if a number is prime
-* @n: number to checked
-*
-* Return: integer value
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked'
+ * @div: The divisor.
+ *
+ * Return: If the number is divisible 0 if the number is not divisible 1
 */
-int _is_prime_number(int n)
+int is_divisible(int num, int div)
 {
-return (check_prime(n, 1));
-}
-
-/**
-* check_prime - Check if number is prime
-* @n: number to calculate the natural square root
-* @i: iterate number
-* Return: 1 for ptime or 0  composite
- */
-int check_prime(int n, int i)
-{
-if (n <= 1)
+if (num % div == 0)
 return (0);
-if (n % i == 0 && i > 1)
-return (0);
-if ((n / i) < i)
-return (1);
-return (check_prime(n, i + 1));
 }
