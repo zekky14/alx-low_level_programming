11918e9c205c12a6c0cb630076480e6d12e4ceea
zekky14 <ezek.ogbeda@gmail.com>
2023-01-11 01:30:07 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,14 @@ char *create_array(unsigned int size, char c)
 unsigned int i;
 char *s;
 
-if(size == 0)
+if (size == 0)
 return (NULL);
 s = malloc(size * sizeof(char));
-if(s == NULL)
+if (s == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
 {
-s[i] =c;
+s[i] = c;
 }
 return (s);
 }
