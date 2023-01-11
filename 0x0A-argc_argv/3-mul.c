193c9510c88fb9ee83577504fd88287a6cb3897a
zekky14 <ezek.ogbeda@gmail.com>
2023-01-10 21:31:29 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,11 +12,11 @@
 int main(int argc, char *argv[])
 {
 int n1 = 0, n2 = 0;
-if (argc ==3)
+if (argc == 3)
 {
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+printf("%d\n", n1 *n2);
 }
 else
 {
