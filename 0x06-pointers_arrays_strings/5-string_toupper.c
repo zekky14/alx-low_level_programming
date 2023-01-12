dfa0309b254f8ec0f82a4c59f5cf793fb97bdfab
zekky14 <ezek.ogbeda@gmail.com>
2023-01-11 21:06:54 -0800

    Lower to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
-* string_topupper -  Changes all lowercase letters
-* of a string touppercase.
-* @str: The string to be changed
-* Return: Apointer to the changed string
-*
-*/
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
+ */
+char *string_toupper(char *s)
+{
 
-char *string_toupper(char *str)
+int i;
+
+i = 0;
+while (*(s + i))
 {
-int index = 0;
-while(str[index])
-{
-if (str[index] >= 'a' && str[index] <= 'z')
-str[index] -= 32;
-index++;
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+*(s + i) -= 'a' - 'A';
+i++;
 }
-return (str);
+return (s);
 }
