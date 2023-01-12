4e6b117167b8a0dea4fe0c9fc85d4ce050a763a2
zekky14 <ezek.ogbeda@gmail.com>
2023-01-11 21:01:18 -0800

    Capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,31 @@
 #include "main.h"
 
 /**
-* cap_string -  Capitalise all words of a string
-* @str: The string to be capitalised
-* Return: Apointer to the changed string
-*
-*/
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
+ */
+char *cap_string(char *s)
+{
+int i = 0, j;
+char a[] = " \t\n,;.!?\"(){}";
 
-char *cap_string(char *str)
+while (*(s + i))
 {
-int index = 0;
-while(str[index])
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
 {
-while (!(str[index] >='a' && str[index] <= 'z'))
-index++;
-if (str[index - 1] == ' ' ||
-str[index -1] == '\t' ||
-str[index -1] == '\n' ||
-str[index -1] == ',' ||
-str[index -1] == ';' ||
-str[index -1] == '.' ||
-str[index -1] == '!' ||
-str[index -1] == '?' ||
-str[index -1] == '"' ||
-str[index -1] == '(' ||
-str[index -1] == ')' ||
-str[index -1] == '{' ||
-str[index -1] == '}' ||
-index == 0)
-str[index] -= 32;
-index++;
+if (i == 0)
+*(s + i) -= 'a' - 'A';
+else
+{
+for (j = 0; j <= 12; j++)
+{
+if (a[j] == *(s + i - 1))
+*(s + i) -= 'a' - 'A';
 }
-return (str);
+}
+}
+i++;
+}
+return (s);
 }
